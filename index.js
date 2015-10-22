db817a275453ef00ac982937142e9bd8e3e92c22
Samuel Bolduc <faramir.sam@gmail.com>
2015-10-22 19:09:52 -0400

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const JsonSocket = require('json-socket');
+
+class SocketClient {
+  constructor(config) {
+    if(!config.port) throw new Error('No port specified');
+    if(!config.host) throw new Error('No host specified');
+    this.serverConfig = {port: config.port, host: config.host};
+  }
+
+  query(msg) {
+    return new Promise((resolve, reject) => {
+      JsonSocket.sendSingleMessageAndReceive(this.serverConfig.port, this.serverConfig.host, msg, (err, res) => {
+        if(err) return reject(err);
+        if(res.status == 'error') return reject((res.e && res.e.msg) ? res.e.msg : res.msg);
+        return resolve(res.data);
+      });
+    });
+  }
+};
+
+module.exports = SocketClient;
